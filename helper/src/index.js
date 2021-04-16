f4c51ef4f685f548fba1f2ce5ec1f139c349c880
Yoon Kim <ykim.imlab@gmail.com>
2021-04-16 14:51:21 +0900

    Update helper

--- helper/src/index.js
+++ helper/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
+// TODO: 도감 사이트에서 데이터 가져오기
+// TODO: 영문 이름 검색
 
 class IVSearch extends React.Component {
     render() {
@@ -8,39 +12,46 @@ class IVSearch extends React.Component {
             <div>
                 <h1>포고 레벨별 IV 검색</h1>
                 <IVSearchMenu/>
-                <IVSearchResult/>
+                <IVSearchResult name="검색 결과"/>
             </div>
         );
     }
 }
 
 class IVSearchMenu extends React.Component {
-    // 검색 버튼
-    // 라디오 버튼 옵션 변경
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+
+    }
 
     render() {
         return (
-            <form>
-                <div className="iv-search-bar">
-                    <input/>
-                    <button>검색</button>
-                </div>
-                <div className="iv-search-option">
-                    <input type="radio" name="type" value="15"/>15레벨
-                    <input type="radio" name="type" value="20" checked/>20레벨
-                    <input type="radio" name="type" value="25"/>25레벨
-                    <input type="radio" name="type" value="30"/>30레벨
-                    <input type="radio" name="type" value="35"/>35레벨
-                </div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <input type="submit" value="검색"/>
             </form>
         );
     }
 }
 
+// TODO: 레벨별 결과를 리스트로 출력
+// TODO: 버튼 없이도 결과를 출력하기
+
 class IVSearchResult extends React.Component {
     render() {
         return (
-            <div>검색 결과</div>
+            <div>{this.props.name}</div>
         );
     }
 }
