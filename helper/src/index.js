c508f9e548ca1804464162ec13437be77c6c5b94
Yoon Kim <ykim.imlab@gmail.com>
2021-04-19 15:38:36 +0900

    Update helper

--- helper/src/index.js
+++ helper/src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 // TODO: 도감 사이트에서 데이터 가져오기
 // TODO: 영문 이름 검색
 
 class IVSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleKeywordChange = this.handleKeywordChange.bind(this);
+        this.state = {keyword: ""};
+    }
+
+    handleKeywordChange(e) {
+        this.setState({keyword: e.target.value});
+    }
+
     render() {
+        const keyword = this.state.keyword;
+
         return (
             <div>
                 <h1>포고 레벨별 IV 검색</h1>
-                <IVSearchMenu/>
-                <IVSearchResult name="검색 결과"/>
+                <IVSearchMenu keyword={keyword}
+                              onKeywordChange={this.handleKeywordChange}/>
+                <IVSearchResult keyword={keyword}/>
             </div>
         );
     }
@@ -21,37 +34,33 @@ class IVSearch extends React.Component {
 class IVSearchMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
-        this.setState({value: e.target.value});
-    }
-
-    handleSubmit(e) {
-
+        this.props.onKeywordChange(e);
     }
 
     render() {
+        const keyword = this.props.keyword;
+
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                <input type="submit" value="검색"/>
+            <form>
+                <input type="text" value={keyword}
+                       onChange={this.handleChange}/>
             </form>
         );
     }
 }
 
 // TODO: 레벨별 결과를 리스트로 출력
-// TODO: 버튼 없이도 결과를 출력하기
 
 class IVSearchResult extends React.Component {
     render() {
+        const keyword = this.props.keyword;
+
         return (
-            <div>{this.props.name}</div>
+            <div>{keyword}</div>
         );
     }
 }
@@ -60,5 +69,5 @@ class IVSearchResult extends React.Component {
 
 ReactDOM.render(
     <IVSearch/>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
